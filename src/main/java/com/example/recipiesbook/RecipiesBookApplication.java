89388c3bfab84d1a2c919a6512a1f6d07ebcee6a
antebuba <ante.bubalo@fsre.sum.ba>
2023-06-14 11:24:59 +0200

    push

--- src/main/java/com/example/recipiesbook/RecipiesBookApplication.java
+++ src/main/java/com/example/recipiesbook/RecipiesBookApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipiesBookApplication {
+public class RecipiesBookApplication{
 
     public static void main(String[] args) {
 
